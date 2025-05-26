ca0efde70c5ae1797424de6c56e83d035172a195
donnlouco <vinicius.lima6@estudante.ifms.edu.br>
2025-05-25 22:11:51 -0300

    cansei de tentar kakaka

--- 42.py
+++ 42.py
@@ -1,11 +1,20 @@
 # 42. Dada uma lista de inteiros, crie uma funcao que identifique os numeros que aparecem mais de uma vez e quantas vezes cada um aparece.
 
 lista1 = [1, 2, 3, 2, 4, 1, 3, 5, 6, 7, 8, 2, 1, 9, 2, 10]
-lista2 = list(set(lista1))
-fora = []
+
+quantide = {}
 
 for i in lista1:
-    for j in lista2:
-        if i == j:
-            fora.append(i)
-print(fora)
+    if i in quantide:
+        quantide[i] += 1
+    else:
+        quantide[i] = 1
+
+for i, j in quantide.items():
+    print(f'o numero {i} aparece {j}')
+    
+
+for i in set(lista1):  # Para cada número único
+    contagem = lista1.count(i)
+    if contagem > 1:
+        print(f"Número {i} aparece {contagem} vezes.")
--- 43.py
+++ 43.py
@@ -1,2 +1,62 @@
 # 43. Implemente um sistema que simule um carrinho de compras: adicao, remocao e total de itens com precos.
 
+carrinho = {}
+soma = 0
+from random import randint
+
+while True:
+    nome_item = input('informe o nome do item que deseja: ').upper()
+    if nome_item == "SAIR":
+        break
+    p = randint(0, 100)
+    preco_item = input(f'O preco do item {nome_item} e R${p}, deseja colocar em seu carrinho? (SIM ou NAO): ').upper()
+    if preco_item == 'SIM':
+        carrinho[nome_item] = {'preco': p}
+    print('Digite sair para sair')
+    
+for j, i in carrinho.items():
+    soma += i['preco']
+    print(j, i)
+print(soma)
+
+# for i, j in carrinho.items():
+#     remover = input(f'Deseja remover o {i}? (NAO para passar) ').upper()
+#     if remover == 'NAO' or 'N':
+#         pass
+#     else:
+#         del carrinho[i]
+
+# print(f'O valor total foi R$ {valor}')  
+# print(f'Seus itens foram: {carrinho}')
+
+
+# from random import randint
+# carrinho = []
+# final = 0
+# while True:
+#     itens = []
+#     preco = []
+#     nome_item = input('informe o nome do item que deseja: ').upper()
+#     if nome_item == "SAIR":
+#         break
+#     p = randint(0, 100)
+#     preco_item = input(f'O preco do item {nome_item} e R${p}, deseja colocar em seu carrinho? (SIM ou NAO): ').upper()
+#     if preco_item == 'SIM':
+#         itens.append(nome_item)
+#         preco.append(p)
+#     carrinho.append(list(zip(itens, preco)))
+#     print('Digite sair para sair')
+    
+
+# print(carrinho)
+# for i in carrinho:
+#     remover = input(f'Deseja remover o {i}? (NAO para passar) ').upper()
+#     if remover == 'SIM' or 'S':
+#         carrinho.remove(i)
+#     else:
+#         pass
+
+
+# print(f'Seus itens foram: {carrinho}')
+# print(f'Seus itens foram: {sum(i for j, i in carrinho)}')
+
