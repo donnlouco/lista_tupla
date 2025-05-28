e252ec0ace80d24e1762f0e836210cc36f308896
Vinicius Lima <vinicius.lima6_estud@C110788.pdc-academico.ifms.tl>
2025-05-27 21:55:13 -0400

    ultimo ex

---
+++ 45.py
@@ -0,0 +1,18 @@
+# 45. Faca uma funcao que receba uma lista de listas e retorne uma lista ”achatada”(flatten).
+
+
+import numpy as np
+lista_aninhada = [[1, 2], [3, 4], [5]]
+array_numpy = np.array(lista_aninhada)
+array_plano = array_numpy.flatten()
+print(array_plano)  # Saída: [1 2 3 4 5]
+
+
+'''
+NumPy, abreviatura de "Numerical Python", é uma biblioteca fundamental para a computação científica em Python.
+ Ela fornece suporte para arrays multidimensionais, que são a base para trabalhar com dados numéricos de forma eficiente
+
+
+
+ 
+'''
